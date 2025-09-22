21ce339ee1e4c3a79957c334c5535a5751f53c74
Kim LaRocca <kim@kimlarocca.com>
2025-09-22 14:32:25 -0400

    update copy button

--- components/ChatMessage.vue
+++ components/ChatMessage.vue
@@ -25,26 +25,14 @@
     </div>
 
     <!-- Copy button (appears on hover) -->
-    <button
+    <Button
       v-if="!message.isUser"
       @click="copyMessage"
-      class="opacity-0 group-hover:opacity-100 transition-opacity p-1 hover:bg-gray-700 rounded"
-      title="Copy message"
-    >
-      <svg
-        class="w-4 h-4 text-gray-400"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-        />
-      </svg>
-    </button>
+      class="opacity-0 group-hover:opacity-100 transition-opacity px-4"
+      aria-label="Copy message"
+      icon="pi pi-copy"
+      v-tooltip="`Copy message`"
+    />
   </div>
 </template>
 
